61d38093fa35f654797e0f1b0899e818068b5f38
alexiwisteria <alexiseblee@outlook.com>
2025-01-24 10:19:24 -0700

    Added Resume Info and Customized formatting

--- src/app/hero.tsx
+++ src/app/hero.tsx
@@ -7,27 +7,32 @@ function Hero() {
     <div className="relative w-full">
       <div className="grid place-items-center min-h-[92vh] px-8">
         <div className="container mx-auto grid place-items-center h-max text-center">
-          <Typography variant="h1" color="blue-gray">
-            My Resume
+          <Typography variant="h1" color="blue-gray" >
+            Aspiring Data Solutions Architect:</Typography>
+          <Typography variant="h1" color="blue-gray" >
+            AI & Backend
           </Typography>
           <Typography
             variant="lead"
             color="gray"
-            className="mt-4 mb-12 w-full md:max-w-full lg:max-w-4xl"
+            className="mt-4 mb-4 w-full md:max-w-full lg:max-w-4xl"
           >
-            Welcome to my professional resume page! Below, you&apos;ll find a
-            comprehensive overview of my skills, qualifications, and experience
-            in the field of web development.
+            Hello and welcome!
           </Typography>
+          <Typography
+              variant="lead"
+              color="gray"
+              className="mb-6 w-full md:max-w-full lg:max-w-4xl"
+          >
+            Here’s what I’m currently focusing on: refreshing my skills in data structures, Big O notation, and interfaces while actively exploring large language models (LLMs). My goal is to integrate these technologies into programs in a way that’s impactful, effective, and ethically responsible.
+          </Typography>
+
           <Typography className="mt-12 mb-4 text-blue-gray-900 font-medium uppercase">
             Connect me on:
           </Typography>
           <div className="gap-2 lg:flex">
             <IconButton variant="text" color="gray">
-              <i className="fa-brands fa-facebook text-lg" />
-            </IconButton>
-            <IconButton variant="text" color="gray">
-              <i className="fa-brands fa-instagram text-lg" />
+              <i className="fa-brands fa-linkedin text-lg" />
             </IconButton>
             <IconButton variant="text" color="gray">
               <i className="fa-brands fa-github text-lg" />
--- src/app/information-section.tsx
+++ src/app/information-section.tsx
@@ -11,149 +11,179 @@ import InfoCard from "@/components/info-card";
 const EDUCATION = [
   {
     icon: AcademicCapIcon,
-    title: "Certified Web Developer - Web Development Institute",
-    date: "2016",
+    title: "Bachelor of Science in Software Engineering - Ensign College",
+    date: "Jan 2025 - Apr 2026",
     children:
-      "This comprehensive program covered HTML5, CSS3, JavaScript, responsive design, server-side scripting, and web security.",
-  },
-  {
-    icon: AcademicCapIcon,
-    title: "Responsive Web Design Certification - FreeCodeCamp",
-    date: "2015",
-    children:
-      "The Responsive Web Design certification signifies competence in designing and developing websites that adapt seamlessly to various screen sizes and devices.",
-  },
-  {
-    icon: AcademicCapIcon,
-    title: "JavaScript Developer Certification - Code Academy",
-    date: "2014",
-    children:
-      "This certification demonstrates advanced proficiency in JavaScript programming, including ES6 features and practical applications.",
-  },
-  {
-    icon: AcademicCapIcon,
-    title: "Bachelor of Science in Computer Science - XYZ University",
-    date: "2014-2016",
-    children:
-      "Relevant Coursework: Data Structures, Algorithms, Web Development, Software Engineering, Database Management.",
+        "Relevant Coursework: Introduction to Programming, Data Structures, Object-Oriented Programming, Web Development, Discrete Math, Technical Writing, Agile Project Management, Statistics.",
   },
 ];
 
 const EXPERIENCE = [
   {
     icon: BriefcaseIcon,
-    title: "Freelancer Web Developer",
-    date: "2023 - PRESENT",
+    title: "Help Desk Technician - Ensign College",
+    date: "Jan 2024 - Present",
     children:
-      "The core of my work involved actual web development. I would write code, design layouts, and create functionality based on the project's specifications.",
+        "Provided QA support during a major WiFi migration, resolving technical issues with ServiceNow and TeamDynamix. Documented network changes and delivered ongoing support to improve ticket resolution rates and user satisfaction.",
   },
   {
     icon: BriefcaseIcon,
-    title: "Technical Team Lead",
-    date: "2021 - 2023",
+    title: "Kennel Technician - Bountiful Animal Hospital",
+    date: "Feb 2022 - Oct 2023",
     children:
-      "I provided strong leadership by overseeing and guiding a team of highly skilled technical professionals.",
+        "Followed strict animal safety protocols, maintaining a zero-incident record. Managed multiple tasks in a fast-paced environment, improving operational efficiency and project completion timelines.",
   },
   {
     icon: BriefcaseIcon,
-    title: "Senior Web Developer",
-    date: "2017 - 2021",
+    title: "Manager - Twisted Sugar",
+    date: "Jan 2020 - Jan 2021",
     children:
-      "Revamped the automated test framework for web services, resulting in a remarkable 90% reduction in debugging and issue resolution time.",
+        "Created an employee handbook and training program, enhancing team efficiency and onboarding processes. Provided tailored customer service, improving loyalty and satisfaction.",
   },
   {
     icon: BriefcaseIcon,
-    title: "Junior Web Developer",
-    date: "2015 - 2017",
+    title: "Team Lead - Research and Business Development Center",
+    date: "Apr 2020 - Jul 2020",
     children:
-      "Developed 10+ responsive websites for clients in a variety of industries.",
+        "Led a team and implemented efficient workflows, self-learned data analytics, and created Excel-based workflows that delivered actionable insights.",
   },
 ];
 
-const SKILLS = [
+const SKILLS = {
+  proficient: [
+    {
+      icon: FireIcon,
+      title: "Proficient Technologies",
+      date: "Technical Skills",
+      children: "Java, Object-Oriented Programming, Agile methodologies.",
+    },
+  ],
+  emerging: [
+    {
+      icon: FireIcon,
+      title: "Emerging Technologies",
+      date: "Technical Skills",
+      children:
+          "Git, JUnit, SQL, ServiceNow, Vite, TypeScript, JavaScript, Python, React, Next.js, MongoDB.",
+    },
+  ],
+};
+
+const EXTRACURRICULARS = [
   {
     icon: FireIcon,
-    title: "Front-End Frameworks",
-    date: "Technical Skills",
+    title: "Coding Club Member - Salt Lake City, Utah",
+    date: "Jan 2025",
     children:
-      "Competent in working with front-end frameworks such as React, Angular, or Vue.js to develop dynamic and responsive web applications with a focus on user experience.",
+        "Collaborating on software projects using Java, Python, and JavaScript. Organizing workshops and participating in hackathons to solve real-world challenges. Mentored peers in debugging techniques, Git workflows, and coding fundamentals.",
   },
   {
     icon: FireIcon,
-    title: "Attention to Detail",
-    date: "Soft Skills",
+    title: "Studio E - Ensign College AI Autograding Project - Scrum Master",
+    date: "Jan 2025",
     children:
-      "Meticulous attention to detail in code quality, user interface design, and testing to ensure error-free and user-friendly web applications.",
-  },
-  {
-    icon: FireIcon,
-    title: "Responsive Web Design",
-    date: "Technical Skills",
-    children:
-      "Skilled in creating responsive layouts using CSS Grid, Flexbox, and media queries. Ensures websites adapt seamlessly to various screen sizes and devices.",
-  },
-  {
-    icon: FireIcon,
-    title: "Time Management",
-    date: "Soft Skills",
-    children:
-      "Excellent time management skills to meet project deadlines, prioritize tasks effectively, and handle multiple projects simultaneously.",
+        "Leading an agile team in AIOps, consistently achieving milestones by integrating advanced AI models to enhance Techlab Grading operations. Optimized data pipelines and employed the OpenAI ChatGPT API for rapid prototyping, enhancing development speed and efficiency. Developed innovative AIOps solutions that improved system reliability and strengthened collaboration between teams, leading to successful tool integrations.",
   },
 ];
 
 export function InformationSection() {
   return (
-    <section className="pb-28 px-8">
-      <div className="grid xl:grid-cols-2 md:grid-cols-1 container gap-20 mx-auto items-start">
-        <div>
-          <div className="mb-10">
-            <Typography color="blue-gray" className="mb-2 text-3xl font-bold">
-              Education & Certifications
-            </Typography>
-            <Typography variant="lead" className="!text-gray-500">
-              See my education history.
-            </Typography>
+      <section className="pb-28 px-8">
+        <div className="container mx-auto items-start">
+          {/* Education Section */}
+          <div className="mb-36">
+            <div className="mb-10">
+              <Typography color="gray" className="mb-2 text-3xl font-bold">
+                Education
+              </Typography>
+              <Typography variant="lead" className="!text-gray-500">
+                See my education history.
+              </Typography>
+            </div>
+            <div className="container mx-auto grid grid-cols-1 gap-16 gap-y-12">
+              {EDUCATION.map((props, idx) => (
+                  <InfoCard key={idx} {...props} />
+              ))}
+            </div>
           </div>
-          <div className="container mx-auto grid grid-cols-1 gap-16 gap-y-12">
-            {EDUCATION.map((props, idx) => (
-              <InfoCard key={idx} {...props} />
-            ))}
+
+          {/* Extracurricular Activities Section */}
+          <div className="mb-36">
+            <div className="mb-10">
+              <Typography color="gray" className="mb-2 text-3xl font-bold">
+                Extracurricular Activities
+              </Typography>
+              <Typography variant="lead" className="!text-gray-500">
+                A glimpse into my collaborative and leadership roles outside of work.
+              </Typography>
+            </div>
+            <div className="container mx-auto grid grid-cols-1 gap-16 gap-y-12">
+              {EXTRACURRICULARS.map((props, idx) => (
+                  <InfoCard key={idx} {...props} />
+              ))}
+            </div>
+          </div>
+
+          {/* Experience Section */}
+          <div className="mb-36">
+            <div className="mb-10">
+              <Typography color="gray" className="mb-2 text-3xl font-bold">
+                Experience
+              </Typography>
+              <Typography variant="lead" className="!text-gray-500">
+                See my professional experience.
+              </Typography>
+            </div>
+            <div className="container mx-auto grid grid-cols-1 gap-16 gap-y-12">
+              {EXPERIENCE.map((props, idx) => (
+                  <InfoCard key={idx} {...props} />
+              ))}
+            </div>
+          </div>
+
+          {/* Skills Section */}
+          <div>
+            <div className="mb-10">
+              <Typography color="gray" className="mb-2 text-3xl font-bold">
+                Skills
+              </Typography>
+              <Typography variant="lead" className="!text-gray-500">
+                Here’s a look at my technical skills.
+              </Typography>
+            </div>
+
+            {/* Proficient Skills */}
+            <div className="mb-12">
+              <Typography
+                  color="gray"
+                  className="mb-2 text-2xl font-semibold"
+              >
+                Proficient Skills
+              </Typography>
+              <div className="container mx-auto grid grid-cols-1 gap-16 gap-y-12 lg:grid-cols-2">
+                {SKILLS.proficient.map((props, idx) => (
+                    <InfoCard key={idx} {...props} />
+                ))}
+              </div>
+            </div>
+
+            {/* Emerging Skills */}
+            <div>
+              <Typography
+                  color="gray"
+                  className="mb-2 text-2xl font-semibold"
+              >
+                Emerging Skills
+              </Typography>
+              <div className="container mx-auto grid grid-cols-1 gap-16 gap-y-12 lg:grid-cols-2">
+                {SKILLS.emerging.map((props, idx) => (
+                    <InfoCard key={idx} {...props} />
+                ))}
+              </div>
+            </div>
           </div>
         </div>
-        <div>
-          <div className="mb-10">
-            <Typography color="blue-gray" className="mb-2 text-3xl font-bold">
-              Experience
-            </Typography>
-            <Typography variant="lead" className="!text-gray-500">
-              See my experience as a developer.
-            </Typography>
-          </div>
-          <div className="container mx-auto grid grid-cols-1 gap-16 gap-y-12">
-            {EXPERIENCE.map((props, idx) => (
-              <InfoCard key={idx} {...props} />
-            ))}
-          </div>
-        </div>
-      </div>
-      <div className="container gap-20 mt-36 mx-auto items-center">
-        <div>
-          <div className="mb-10">
-            <Typography color="blue-gray" className="mb-2 text-3xl font-bold">
-              Skills
-            </Typography>
-            <Typography variant="lead" className="!text-gray-500">
-              Check out my technical and soft skills.
-            </Typography>
-          </div>
-          <div className="container mx-auto grid grid-cols-1 gap-16 gap-y-12 lg:grid-cols-2">
-            {SKILLS.map((props, idx) => (
-              <InfoCard key={idx} {...props} />
-            ))}
-          </div>
-        </div>
-      </div>
-    </section>
+      </section>
   );
 }
 
