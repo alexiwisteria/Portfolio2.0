ed00d6b36e5c9aaf07d5f268e19a2fd45799e9ec
alexiwisteria <alexiseblee@outlook.com>
2025-01-29 12:52:25 -0700

    Finishing Touches

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -7,10 +7,13 @@ export function Footer() {
                 <div className="mt-16 flex flex-wrap items-center justify-center gap-y-4 border-t border-gray-200 py-6 md:justify-between">
                     <p className="text-center font-normal text-gray-700">
                         &copy; Built with Next.js, Tailwind CSS, and Material Tailwind by Alexis Binch Lee.
+                        <a href="https://giphy.com/carolynnyoe"> Gif created by @carolynnyoe on Giphy.
+                        </a>
                     </p>
                 </div>
             </div>
         </footer>
     );
 }
+
 export default Footer;
