2952018fa5a70801df4c2b12fd97f418743d8548
alexiwisteria <alexiseblee@outlook.com>
2025-01-29 14:22:55 -0700

    More Responsive

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import "./globals.css";
-import {Space_Mono} from "next/font/google"; // Import Space Mono
-import {Layout} from "@/components";
+import { Space_Mono } from "next/font/google"; // Import Space Mono
+import { Layout } from "@/components";
 import TableOfContents from "@/components/TableOfContents";
 
 const spaceMono = Space_Mono({
@@ -10,21 +10,18 @@ const spaceMono = Space_Mono({
     display: "swap",
 });
 
-
-export default function RootLayout({
-                                       children,
-                                   }: {
-    children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
-        <html lang="en" className={`${spaceMono.className}`}>
+        <html lang="en" className={`${spaceMono.className} h-full`}>
         <head>
-            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1"/>
             <link rel="shortcut icon" href="/favicon.png" type="image/png"/>
         </head>
-        <body style={{backgroundColor: "#FAFAFA"}}>
+        <body className="min-h-screen flex flex-col w-full bg-gray-50">
         <Layout>
-        {children}
+            <div className="w-full max-w-screen-xl mx-auto p-4 md:p-6 lg:p-8">
+                {children}
+            </div>
         </Layout>
         </body>
         </html>
